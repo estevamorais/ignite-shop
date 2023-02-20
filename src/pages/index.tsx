ecf7113e414329d5c3d69c087d0a2b24763e3763
estevamorais <demoraisestevam@gmail.com>
2023-02-20 08:34:25 -0300

    Setup global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    backgroundColor: '$green300',
+  },
+})
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <Button>
+      <span>Teste</span> Enviar
+    </Button>
   )
 }
