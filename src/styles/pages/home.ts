4f6dc62040f4cfa806956beab46e21f2e961dfda
estevamorais <demoraisestevam@gmail.com>
2023-02-26 15:28:23 -0300

    Improving UX: Add margin right to slider

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -9,8 +9,9 @@ export const HomeContainer = styled('main', {
 export const HomeSlider = styled('div', {
   minHeight: 656,
   marginLeft: 'auto',
+  marginRight: '5rem',
   width: '100%',
-  maxWidth: 'calc(100vw - ((100vw - 1180px) /2))',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) /2) - 5rem)',
   overflow: 'visible !important', // Subscribe keen slider
 })
 
